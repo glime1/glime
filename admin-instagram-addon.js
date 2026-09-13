aaab30dd26f9db4cd7845a3b36d7f7403d5768db
glime1 <connect.glime@gmail.com>
2026-09-13 22:41:04 +0530

    Update admin-instagram-addon.js

--- admin-instagram-addon.js
+++ admin-instagram-addon.js
@@ -544,6 +544,15 @@
         saveModuleAccess
       );
   }
+  
+  function escapeHtml(value) {
+  return String(value ?? '')
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+  }
 
 
   /* =====================================================
